9c0c9e4b00d7f0811e9ed22b5de3589cf21a8154
iabbene <ianabbene@gmail.com>
2023-06-29 12:38:56 -0500

    Remove unnecessary comments in OrderDetail

--- src/components/OrderDetail/OrderDetail.jsx
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -19,46 +19,13 @@ export default function OrderDetail({ order, handleChangeQty, handleCheckout }) 
         <div className="order-detail">
             Cart
             <div className="section-heading">
-                {/* {order.isPaid ?
-                    <span>ORDER <span className="smaller">{order.orderId}</span></span>
-                    :
-                    <span>NEW ORDER</span>
-                } */}
+
                 <span>{new Date(order.updatedAt).toLocaleDateString()}</span>
             </div>
             <div>
                 {cartItems}
             </div>
             <button onClick={() => handleCheckout()}>Checkout</button>
-
-            {/* <div className="cart-item-container flex-ctr-ctr flex-col scroll-y">
-                {cartItems.length ?
-                    <>
-                        {cartItems}
-                        <section className="total">
-                            {order.isPaid ?
-                                <span className="right">TOTAL&nbsp;&nbsp;</span>
-                                :
-                                <button
-                                    className="btn-sm"
-                                    onClick={handleCheckout}
-                                    disabled={!cartItems.length}
-                                >CHECKOUT</button>
-                            }
-                            <span>{order.totalQty}</span>
-                            <span className="right">${order.orderTotal.toFixed(2)}</span>
-                        </section>
-                    </>
-                    :
-                    <div className="hungry">Hungry?</div>
-                } */}
         </div >
     );
 }
-// export default function OrderDetail() {
-//     return (
-// <>
-// line items will go here aka order detail page
-// </>
-//     );
-// }
